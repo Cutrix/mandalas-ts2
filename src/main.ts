6bc0a0c63d80f2d5eed8637ddbb21f4012059e6d
cutr21 <houessinonlandry@gmail.com>
2020-07-11 08:54:41 +0000

    build: bootstrap-vue bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "jquery";
 import Vue from "vue";
 import App from "./App.vue";
@@ -5,6 +8,9 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
